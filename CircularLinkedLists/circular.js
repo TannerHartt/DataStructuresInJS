29899999dcfaf68b3b49948a27676b52b8b1494c
Tanner Hart <tannerhart24@yahoo.com>
2023-02-24 16:11:59 -0800

    Added basic pop, push, shift, unshift functions and full test coverage for each

--- CircularLinkedLists/circular.js
+++ CircularLinkedLists/circular.js
@@ -1,8 +1,9 @@
+import LinkedList from '../Linked-Lists/LinkedList';
 export class Node {
     constructor(data, next = null, prev = null) {
         this.data = data;
         this.next = next;
-        this.prev = prev;
+        // this.prev = prev;
     }
 
     getData() { return this.data; }
@@ -13,9 +14,9 @@ export class Node {
 
     setNext(next) { this.next = next; }
 
-    getPrev() { return this.prev; }
+    // getPrev() { return this.prev; }
 
-    setPrev(prev) { this.prev = prev; }
+    // setPrev(prev) { this.prev = prev; }
 }
 
 export default class CircularLinkedList {
@@ -30,5 +31,90 @@ export default class CircularLinkedList {
             this.tail = node;
             this.size = 1;
         }
+    }
+    // constructor() {
+    //     super();
+    // }
+
+
+    getSize() { return this.size }
+
+    getHead() { return this.head }
+
+    getTail() { return this.tail }
+
+    clearList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    // o(1)
+    // Insert a node the end of the list
+    push(value){
+        let node = new Node(value, this.head); // Creates a new node with the head as its next.
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+        } else { // Update tail to new node
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.size++;
+        return this;
+    }
+
+    // o(n)
+    // Remove the last element and return it.
+    pop() {
+        if (!this.head || this.size === 0) return null; // If list is empty
+        if (this.size === 1) { // If list has one element
+            let temp = this.head;
+            this.head = null;
+            this.tail = null;
+            this.size--;
+            return temp;
+        }
+        // If list has more than one element
+        let previous = this.head;
+        let current = this.head.next;
+
+        while (current !== this.tail) { // Loop until tail is found
+            previous = current;
+            current = current.next;
+        }
+        previous.next = this.head; // Create circular loop back to head
+        this.tail = previous; // Update tail
+        current.next = null; // Remove last pointer
+        this.size--; // Decrement size
+        return current; // Return the removed node
+    }
+
+    //O(1)
+    // Remove first node of the list and return it
+    shift() {
+        if (!this.head || this.size === 0) return null;
+        let temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        this.tail.next = this.head
+        this.size--;
+        return temp;
+    }
+
+    // o(1)
+    // Insert a node at the beginning
+    unshift(value) {
+        if (!this.head || this.size === 0) { 
+            let node = new Node(value);
+            this.head = node;
+            this.tail = node;
+            this.tail.next = this.head;
+        } else {
+            this.head = new Node(value, this.head);
+            this.tail.next = this.head;
+        }
+        this.size++;
+        return this;
     }
 }
--- CircularLinkedLists/circular.test.js
+++ CircularLinkedLists/circular.test.js
@@ -1,25 +1,105 @@
 import CircularLinkedList from './circular';
 import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import LinkedList from '../Linked-Lists/LinkedList';
 
-describe("Doubly LinkedList", () => {
-    let linkedList;
+describe.only("Doubly LinkedList", () => {
+    let list;
 
     beforeEach(() => {
-        linkedList = new CircularLinkedList();
+        list = new CircularLinkedList();
     });
 
     afterEach(() => { 
-        linkedList.clearList();
+        list.clearList();
     });
 
     it("Should push a new node on to the list", () => {
-        
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        expect(list.getSize()).toBe(3);
+        expect(list.getHead().data).toBe(1);
+        expect(list.getTail().data).toBe(3);
+        expect(list.getTail().next.data).toBe(1);
     });
 
+    it("Should pop a node off the end of a list and return it", () => {
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        expect(list.getSize()).toBe(3);
+        expect(list.getHead().data).toBe(1);
+        expect(list.getTail().data).toBe(3);
+
+        expect(list.pop().data).toBe(3);
+        expect(list.getSize()).toBe(2);
+        expect(list.getHead().data).toBe(1);
+        expect(list.getTail().data).toBe(2);
+
+        expect(list.pop().data).toBe(2);
+        expect(list.getSize()).toBe(1);
+        expect(list.getHead().data).toBe(1);
+        expect(list.getTail().data).toBe(1);
+
+        expect(list.pop().data).toBe(1);
+        expect(list.getSize()).toBe(0);
+        expect(list.getHead()).toBeFalsy();
+        expect(list.getTail()).toBeFalsy();
+
+        expect(list.pop()).toBeFalsy(); // Pop on an empty list
+    });
+
+    it("Should insert a node at the beginning", () => {
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        expect(list.getSize()).toBe(3);
+        expect(list.getHead().data).toBe(1);
+        expect(list.getTail().data).toBe(3);
+        expect(list.getTail().next.data).toBe(1);
+
+        list.unshift(6);
+        expect(list.getSize()).toBe(4);
+        expect(list.getHead().data).toBe(6);
+        expect(list.getTail().data).toBe(3);
+        expect(list.getTail().next.data).toBe(6);
+
+        list.unshift(3);
+        expect(list.getSize()).toBe(5);
+        expect(list.getHead().data).toBe(3);
+        expect(list.getTail().data).toBe(3);
+        expect(list.getTail().next.data).toBe(3);
+
+        list.clearList();
+        list.unshift(2);
+        expect(list.getSize()).toBe(1);
+        expect(list.getHead().data).toBe(2)
+        expect(list.getTail().data).toBe(2)
+        expect(list.getTail().next.data).toBe(2)
+    });
+
+    it.only("Should remove the node at the beginning and return it", () => {
+        list.push(1);
+        list.push(2);
+        expect(list.getSize()).toBe(2);
+        expect(list.getHead().data).toBe(1);
+        expect(list.getTail().data).toBe(2);
+
+        expect(list.shift().data).toBe(1);
+        expect(list.getSize()).toBe(1);
+        expect(list.getHead().data).toBe(2);
+        expect(list.getTail().data).toBe(2);
+        expect(list.getTail().next.data).toBe(2);
+
+        expect(list.shift().data).toBe(2);
+        expect(list.getSize()).toBe(0);
+        expect(list.getHead().data).toBe(2);
+        expect(list.getTail().data).toBe(2);
+        expect(list.getTail().next.data).toBe(2);
+
+
+        list.clearList();
+        expect(list.shift()).toBeNull();
+    });
 });
-
-
-
-
-
 
