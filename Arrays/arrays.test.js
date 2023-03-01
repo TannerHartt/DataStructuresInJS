f3aa8f76a178ceab583b8824c5ee3d0626f77c53
Tanner Hart <tannerhart24@yahoo.com>
2023-03-01 12:44:41 -0800

    Wrote a test for radix sort

--- Arrays/arrays.test.js
+++ Arrays/arrays.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, it, beforeEach, afterEach } from 'vitest';
-import { mergeSort, binarySearchInArray, insertionSort, insertionSort2, bubbleSort, selectionSort, quickSort } from './arrays';
+import { mergeSort, binarySearchInArray, insertionSort, insertionSort2, bubbleSort, selectionSort, quickSort, radixSort } from './arrays';
 
-describe("Arrays", () => {
+describe.only("Arrays", () => {
     let array;
 
     beforeEach(() => {
@@ -28,5 +28,20 @@ describe("Arrays", () => {
 
         expect(sorted.length).toBe(8);
         expect(sorted).toEqual([1,2,3,4,5,7,9,10]);
+    });
+
+    it("Should sort an array using radix sort method", () => {
+        array = [1, 4, 2, 7, 5, 9, 3, 10];
+
+        let sorted = radixSort(array);
+
+        expect(sorted.length).toBe(8);
+        expect(sorted).toEqual([1,2,3,4,5,7,9,10]);
+
+        array = [15554, 1353, 562, 3734, 2346, 3, 66, 123, 0];
+
+        sorted = radixSort(array);
+        expect(sorted.length).toBe(9);
+        expect(sorted).toEqual([0,3,66,123,562,1353,2346,3734,15554]);
     });
 });
